294c83b2ef48096bbd03c8cde5b7729715617421
Vidya <bathija.vidya@gmail.com>
2025-07-29 21:14:42 +0530

    RequestBuilder Changes

--- src/main/java/utils/RequestBuilder.java
+++ src/main/java/utils/RequestBuilder.java
@@ -6,13 +6,14 @@ import io.restassured.specification.RequestSpecification;
 import service.AuthService;
 
 public class RequestBuilder {
-
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String AUTH_HEADER = "Authorization";
     public static RequestSpecification withAuth(String URI) {
         return RestAssured.given()
                 .baseUri(URI)
                 .contentType(ContentType.JSON)
-                .accept("application/json")
-                .header("Authorization", "Bearer " + fileReader.readFromFile("output.json"));
+                .accept(APPLICATION_JSON)
+                .header(AUTH_HEADER, "Bearer " + fileReader.readFromFile("output.json"));
     }
 
     public static RequestSpecification withoutAuth(String URI) {
